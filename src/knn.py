817ee856e8e7de130ecb98b3b22369f427d922de
dharmahoy <dharmahoy@email.arizona.edu>
2022-05-01 22:03:44 -0700

    update knn

--- src/knn.py
+++ src/knn.py
@@ -27,7 +27,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random
 knnModel = KNeighborsClassifier(n_neighbors=5)
 knnModel.fit(X_train,y_train)
 
-# view the coefficent of determination of the model
+# view the mean accuracy on the given test data and variables
 score = knnModel.score(X_train, y_train)
 print("score:", score)
 
